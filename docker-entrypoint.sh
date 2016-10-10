a2519703decd0482ac80b87b202a17e50b4b4874
julian harley <julian@touchsurgery.com>
2016-10-10 15:39:21 +0100

    add test to entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
+python manage.py test
 python manage.py runserver
